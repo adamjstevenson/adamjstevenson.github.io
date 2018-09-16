9b840553750ab76813e1aafcf0d5da5f862d6915
Adam Stevenson <adamjstevenson@gmail.com>
2018-09-16 12:25:24 -0700

    clean up a bit

--- js/aws-revenue-by-year.js
+++ js/aws-revenue-by-year.js
@@ -7,7 +7,7 @@ var config = {
       "2015",
       "2016",
       "2017"
-    ], 
+    ],
     datasets: [
       {
         label: "AWS Revenue (in millions)",
@@ -17,7 +17,7 @@ var config = {
         borderWidth: 1
       }
     ]
-  }, 
+  },
   options: {
     plugins: {
       deferred: {
@@ -33,9 +33,3 @@ window.onload = function() {
   const ctx = document.getElementById("aws-chart").getContext("2d");
   window.myLine = new Chart(ctx, config);
 };
-
-// new Chart(
-//   document.getElementById("chartjs-1"),
-//   {
-//     "type":"bar","data":{"labels":["January","February","March","April","May","June","July"],
-//     "datasets":[{"label":"My First Dataset","data":[65,59,80,81,56,55,40],"fill":false,"backgroundColor":["rgba(255, 99, 132, 0.2)","rgba(255, 159, 64, 0.2)","rgba(255, 205, 86, 0.2)","rgba(75, 192, 192, 0.2)","rgba(54, 162, 235, 0.2)","rgba(153, 102, 255, 0.2)","rgba(201, 203, 207, 0.2)"],"borderColor":["rgb(255, 99, 132)","rgb(255, 159, 64)","rgb(255, 205, 86)","rgb(75, 192, 192)","rgb(54, 162, 235)","rgb(153, 102, 255)","rgb(201, 203, 207)"],"borderWidth":1}]},"options":{"scales":{"yAxes":[{"ticks":{"beginAtZero":true}}]}}});
